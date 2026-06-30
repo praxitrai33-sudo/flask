0edc528c02b7b69a2f93cf823c7d671091e23dcb
prachit <praxitrai33@gmail.com>
2026-06-30 14:40:13 +0545

    Updated user model

---
+++ peer-to-peer marketplace/models/user.py
@@ -0,0 +1,30 @@
+from app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    fullname = db.Column(db.String(100), nullable=False)
+
+    email = db.Column(db.String(100), unique=True, nullable=False)
+
+    password = db.Column(db.String(255), nullable=False)
+
+    phone = db.Column(db.String(20))
+
+    course = db.Column(db.String(100))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
